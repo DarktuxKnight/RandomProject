74753b48ed5ef01939c2146d8bc0c16e391d089b
Darktux <earnest.soul@gmail.com>
2018-02-28 00:01:05 -0600

    Generic commit!

--- PItoN.py
+++ PItoN.py
@@ -1,3 +1,24 @@
 import string
 import random
 
+pilength = random.randint(4,10)
+#print(pilength)
+
+temp = 22/7
+temp = str(temp)
+x,y = temp.split('.')
+x = x + '.'
+
+numx = 22
+decstring = ''
+for i in range(0,pilength):
+    tempmod = numx%7
+    tempmod = int(str(tempmod) + str('0'))
+    print(tempmod)
+
+    dec,other = str(tempmod/7).split('.')
+    decstring = str(decstring) + str(dec)
+
+    numx = int(str(tempmod%7) + str('0'))
+
+print(str(x) + str(decstring))
